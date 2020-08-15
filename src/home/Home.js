bab62b6e17c38769251ce50b243f4f742c5bc5a2
Minh Le <lenguyetminh1998@gmail.com>
2020-08-15 14:52:41 -0400

    Add stuff

--- src/home/Home.js
+++ src/home/Home.js
@@ -8,26 +8,26 @@ import ListItem from "@material-ui/core/ListItem";
 import Divider from "@material-ui/core/Divider";
 import ListItemText from "@material-ui/core/ListItemText";
 import Typography from "@material-ui/core/Typography";
-import IconButton from '@material-ui/core/IconButton';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import IconButton from "@material-ui/core/IconButton";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 
 import { FaPen } from "react-icons/fa";
 
-/* Function to render a row under track your journey */ 
+/* Function to render a row under track your journey */
 function LocationItem(props) {
   return (
     <List>
       <ListItem alignItems="flex-start">
         <ListItemText
-          primary= {props.date + " | " + props.location}
+          primary={props.date + " | " + props.location}
           secondary={
             <React.Fragment>
               <Typography variant="body2" color="textPrimary"></Typography>
               People met: {props.people}
             </React.Fragment>
           }
-        /> 
-      <ListItemSecondaryAction>
+        />
+        <ListItemSecondaryAction>
           <IconButton edge="end" aria-label="comments">
             <FaPen className="icon" />
           </IconButton>
@@ -38,48 +38,87 @@ function LocationItem(props) {
   );
 }
 
-/* Main default class home */ 
+/* Main default class home */
 class Home extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      storage: [], 
-      address: '3601 Walnut St, Philadelphia, PA 19104', 
-
-    }
+      storage: [],
+      address: "",
+      people: "",
+      date: "",
+    };
+    // !IMPORTANT! bind all functions call to recognize this keyword
+    this.handleChangeAddress = this.handleChangeAddress.bind(this);
+    this.handleChangePeople = this.handleChangePeople.bind(this);
+    this.handleChangeDate = this.handleChangeDate.bind(this);
+    this.addItem = this.addItem.bind(this);
   }
 
-  /* One component mounted, get user current location */ 
-  componentDidMount(){
-    if ("geolocation" in navigator){
-      navigator.geolocation.getCurrentPosition(function(position) {
+  /* One component mounted, get current location */
+  componentDidMount() {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(function (position) {
         console.log("Latitude is :", position.coords.latitude);
         console.log("Longitude is :", position.coords.longitude);
       });
     }
   }
 
-  /* Function to add an address, location and people for Add Location button */ 
-  addItem(){
-    console.log('This is clicked');
+  handleChangeAddress(event) {
+    this.setState({ address: event.target.value });
+  }
+
+  handleChangePeople(event) {
+    this.setState({ people: event.target.value });
+  }
+
+  handleChangeDate(event) {
+    this.setState({ date: event.target.value });
+  }
+
+  /* Add address*/
+  addItem() {
+    console.log(this.state);
   }
 
   render() {
     return (
       <div>
-      <Nav title="Track your journey" subtitle="Record location and people you've met." />
-      <div className="Home">
-        <input type="text" placeholder="Address location" value={this.state.address} />
-        <input type="text" placeholder="People at location" value={this.state.people} />
-        <input type="date" value={this.state.date} />
-        <button className="button default left full"> Search </button>
-        <button className="button left full" onClick={this.addItem}> Add Location </button>
-        <div className="timeline">
-           {this.state.storage.map(s => 
-           <LocationItem date={s.date} location={s.location} people={s.people} />)}
+        <Nav
+          title="Track your journey"
+          subtitle="Record location and people you've met."
+        />
+        <div className="Home">
+          <input
+            type="text"
+            placeholder="Address location"
+            value={this.state.address}
+            onChange={this.handleChangeAddress}
+          />
+          <input
+            type="text"
+            placeholder="People at location"
+            defaultValue={this.state.people}
+            onChange={this.handleChangePeople}
+          />
+          <input type="date" onChange={this.handleChange} />
+          <button className="button default left full"> Search </button>
+          <button className="button left full" onClick={this.addItem}>
+            {" "}
+            Add Location{" "}
+          </button>
+          <div className="timeline">
+            {this.state.storage.map((s) => (
+              <LocationItem
+                date={s.date}
+                location={s.location}
+                people={s.people}
+              />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
     );
   }
 }
