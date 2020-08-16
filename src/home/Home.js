5cd2a3d4a6281a37f0302d6fd5a4dc85203d30c3
Minh Le <lenguyetminh1998@gmail.com>
2020-08-16 11:18:35 -0400

    Add in dialog

--- src/home/Home.js
+++ src/home/Home.js
@@ -3,8 +3,6 @@ import "./Home.css";
 import Nav from "../nav/Nav";
 import Geocode from "react-geocode";
 
-import FormDialog from './Dialog';
-
 /** List component */
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
@@ -20,182 +18,16 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import TextField from '@material-ui/core/TextField';
 
 
-import { FaPen } from "react-icons/fa";
+import { FaPen, FaThinkPeaks } from "react-icons/fa";
 
-function handleEdit(){
-  console.log("this is called!");
-  return <FormDialog />
-}
+
+
 
 /* Function to render a row under track your journey */
 function LocationItem(props) {
-  return (
-    <List>
-      <ListItem alignItems="flex-start">
-        <ListItemText
-          primary={props.date + " | " + props.address}
-          secondary={
-            <React.Fragment>
-              <Typography variant="body2" color="textPrimary"></Typography>
-              People met: {props.people}
-            </React.Fragment>
-          }
-        />
-        <ListItemSecondaryAction>
-          <IconButton edge="end" aria-label="comments">
-            <FaPen className="icon" onClick={handleEdit}/>
-          </IconButton>
-        </ListItemSecondaryAction>
-      </ListItem>
-      <Divider component="li" />
-    </List>
-  );
-}
-/* Main default class home */
-// class Home extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       storage: [],
-//       address: "",
-//       people: "",
-//       date: "",
-//       errorMessage: "",
-//     };
-//     // !IMPORTANT! bind all functions call to recognize this keyword
-//     this.handleChangeAddress = this.handleChangeAddress.bind(this);
-//     this.handleChangePeople = this.handleChangePeople.bind(this);
-//     this.handleChangeDate = this.handleChangeDate.bind(this);
-//     this.setDefaultAddress = this.setDefaultAddress.bind(this);
-//     this.storeItem= this.storeItem.bind(this); 
-//     this.addItem = this.addItem.bind(this);
-//   }
-
-//   setDefaultAddress(latitude, longitude){
-//     Geocode.fromLatLng(latitude, longitude).then(
-//       response => {
-//         const a = response.results[0].formatted_address;
-//         this.setState({
-//           address: a
-//         })
-//       },
-//       error => {
-//         console.error(error);
-//       });
-//   }
-
-//   /* One component mounted, get current location */
-//   componentDidMount() {
-//     console.log(window.localStorage); 
-//     const localAddress =  window.localStorage.getItem("storage"); 
-//     if (localAddress){
-//       this.setState({
-//         storage: JSON.parse(localAddress)
-//       })
-//     }
-
-//     // Get API key 
-//     Geocode.setApiKey("AIzaSyDjrq68D6YESU7xYbaqIElmuOjFBVqJ3Rc"); 
-//     const success = position => {
-//       const latitude = position.coords.latitude;
-//       const longitude = position.coords.longitude;
-//       this.setDefaultAddress(latitude, longitude);
-//     };
-//     const error = () => {
-//       console.log("Unable to retrieve your location");
-//     };
-//     navigator.geolocation.getCurrentPosition(success, error);
-//   }
-
-//   handleChangeAddress(event) {
-//     this.setState({ address: event.target.value });
-//   }
-
-//   handleChangePeople(event) {
-//     this.setState({ people: event.target.value });
-//   }
-
-//   handleChangeDate(event) {
-//     this.setState({ date: event.target.value });
-//   }
-  
-//   storeItem(key, val){
-//     window.localStorage.setItem(key, JSON.stringify(val));
-//   }
-
-//   /* Add address*/
-//   addItem() {
-//     if (this.state.address && this.state.date) {
-//       const locationItem = {
-//         address: this.state.address,
-//         people: this.state.people,
-//         date: this.state.date,
-//       };
-//       const joined = this.state.storage.concat(locationItem);
-//       this.setState({
-//         storage: joined,
-//         errorMessage: "",
-//       }, function(){
-//         this.storeItem("storage", this.state.storage);
-//       });
-//     } else {
-//       const error = "Please fill in the address and date in the input";
-//       this.setState({ errorMessage: error });
-//     }
-//   }
-
-//   render() {
-//     const locationItems = this.state.storage.map((v) => {
-//       return (
-//         <LocationItem
-//           key={v.address}
-//           address={v.address}
-//           date={v.date}
-//           people={v.people}
-//         />
-//       );
-//     });
-
-//     return (
-//       <div>
-//         <Nav
-//           title="Track your journey"
-//           subtitle="Record location and people you've met."
-//         />
-//         <div className="Home">
-//           <div className="errorMessage">{this.state.errorMessage}</div>
-//           <input
-//             type="text"
-//             placeholder="Address location"
-//             value={this.state.address}
-//             onChange={this.handleChangeAddress}
-//           />
-//           <input
-//             type="text"
-//             placeholder="People at location"
-//             defaultValue={this.state.people}
-//             onChange={this.handleChangePeople}
-//           />
-//           <input type="date" onChange={this.handleChangeDate} />
-//           <button className="button default left full"> Search </button>
-//           <button className="button left full" onClick={this.addItem}>
-//             {" "}
-//             Add Location{" "}
-//           </button>
-//           <div className="timeline">{locationItems}</div>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// export default Home;
-
-
-
-export default function AlertDialog() {
   const [open, setOpen] = React.useState(false);
 
   const handleClickOpen = () => {
@@ -208,31 +40,204 @@ export default function AlertDialog() {
 
   return (
     <div>
-      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-        Open alert dialog
-      </Button>
       <Dialog
         open={open}
         onClose={handleClose}
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
       >
-        <DialogTitle id="alert-dialog-title">{"Use Google's location service?"}</DialogTitle>
+        <DialogTitle id="alert-dialog-title">{"Edit your location"}</DialogTitle>
         <DialogContent>
           <DialogContentText id="alert-dialog-description">
-            Let Google help apps determine location. This means sending anonymous location data to
-            Google, even when no apps are running.
+            Edit your information below. Or you can delete it by clicking delete.
           </DialogContentText>
+          <TextField
+            margin="dense"
+            id="location"
+            label="Address"
+            type="text"
+            fullWidth
+          />
+           <TextField
+            margin="dense"
+            id="people"
+            label="People"
+            type="text"
+            fullWidth
+          />
+           <TextField
+            margin="dense"
+            id="date"
+            type="date"
+            fullWidth
+          />
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose} color="primary">
-            Disagree
+            Cancel
           </Button>
           <Button onClick={handleClose} color="primary" autoFocus>
-            Agree
+            Save
           </Button>
         </DialogActions>
       </Dialog>
+      <List>
+      <ListItem alignItems="flex-start">
+        <ListItemText
+          primary={props.date + " | " + props.address}
+          secondary={
+            <React.Fragment>
+              <Typography variant="body2" color="textPrimary"></Typography>
+              People met: {props.people}
+            </React.Fragment>
+          }
+        />
+        <ListItemSecondaryAction>
+          <IconButton edge="end" aria-label="comments">
+            <FaPen className="icon" onClick={handleClickOpen}/>
+          </IconButton>
+        </ListItemSecondaryAction>
+      </ListItem>
+      <Divider component="li" />
+    </List>
     </div>
   );
-};
+}
+/* Main default class home */
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      storage: [],
+      address: "",
+      people: "",
+      date: "",
+      errorMessage: "",
+    };
+    // !IMPORTANT! bind all functions call to recognize this keyword
+    this.handleChangeAddress = this.handleChangeAddress.bind(this);
+    this.handleChangePeople = this.handleChangePeople.bind(this);
+    this.handleChangeDate = this.handleChangeDate.bind(this);
+    this.setDefaultAddress = this.setDefaultAddress.bind(this);
+    this.storeItem= this.storeItem.bind(this); 
+    this.addItem = this.addItem.bind(this);
+  }
+
+  setDefaultAddress(latitude, longitude){
+    Geocode.fromLatLng(latitude, longitude).then(
+      response => {
+        const a = response.results[0].formatted_address;
+        this.setState({
+          address: a
+        })
+      },
+      error => {
+        console.error(error);
+      });
+  }
+
+  /* One component mounted, get current location */
+  componentDidMount() {
+    console.log(window.localStorage); 
+    const localAddress =  window.localStorage.getItem("storage"); 
+    if (localAddress){
+      this.setState({
+        storage: JSON.parse(localAddress)
+      })
+    }
+
+    // Get API key 
+    Geocode.setApiKey("AIzaSyDjrq68D6YESU7xYbaqIElmuOjFBVqJ3Rc"); 
+    const success = position => {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      this.setDefaultAddress(latitude, longitude);
+    };
+    const error = () => {
+      console.log("Unable to retrieve your location");
+    };
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+
+  /* Handlers for input forms */ 
+  handleChangeAddress(event) {
+    this.setState({ address: event.target.value });
+  }
+  handleChangePeople(event) {
+    this.setState({ people: event.target.value });
+  }
+  handleChangeDate(event) {
+    this.setState({ date: event.target.value });
+  }
+  storeItem(key, val){
+    window.localStorage.setItem(key, JSON.stringify(val));
+  }
+  
+
+  /* Add address*/
+  addItem() {
+    if (this.state.address && this.state.date) {
+      const locationItem = {
+        address: this.state.address,
+        people: this.state.people,
+        date: this.state.date,
+      };
+      const joined = this.state.storage.concat(locationItem);
+      this.setState({
+        storage: joined,
+        errorMessage: "",
+      }, function(){
+        this.storeItem("storage", this.state.storage);
+      });
+    } else {
+      const error = "Please fill in the address and date in the input";
+      this.setState({ errorMessage: error });
+    }
+  }
+
+  render() {
+    const locationItems = this.state.storage.map((v) => {
+      return (
+        <LocationItem
+          key={v.address}
+          address={v.address}
+          date={v.date}
+          people={v.people}
+        />
+      );
+    });
+
+    return (
+      <div>
+        <Nav
+          title="Track your journey"
+          subtitle="Record location and people you've met."
+        />
+        <div className="Home">
+          <div className="errorMessage">{this.state.errorMessage}</div>
+          <input
+            type="text"
+            placeholder="Address location"
+            value={this.state.address}
+            onChange={this.handleChangeAddress}
+          />
+          <input
+            type="text"
+            placeholder="People at location"
+            defaultValue={this.state.people}
+            onChange={this.handleChangePeople}
+          />
+          <input type="date" onChange={this.handleChangeDate} />
+          <button className="button left full"> Visualize </button>
+          <button className="button default left full" onClick={this.addItem}>
+            {" "}
+            Add Location{" "}
+          </button>
+          <div className="timeline">{locationItems}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
