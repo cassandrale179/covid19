102cbddcdbac1b727435d02cc3c13dcccbaa9bac
Minh Le <lenguyetminh1998@gmail.com>
2020-08-21 22:43:08 -0400

    stuff

--- src/home/AutoCompleteAddressForm.js
+++ src/home/AutoCompleteAddressForm.js
@@ -15,54 +15,45 @@ const loadScript = (url, callback) => {
   }
   script.src = url;
   document.getElementsByTagName("head")[0].appendChild(script);
-  const latlngStorage = window.localStorage.getItem('latlngStorage'); 
-  const zipCodeStorage = window.localStorage.getItem('zipCodeStorage');
-  if (!latlngStorage){
-    window.localStorage.setItem('latlngStorage', JSON.stringify([]));
-  }
-  if (!zipCodeStorage){
-    window.localStorage.setItem('zipCodeStorage', JSON.stringify([]));
-  }
-  console.log(window.localStorage);
 };
 
-function handleScriptLoad(updateQuery, autoCompleteRef) {
+function handleScriptLoad(updateAddress, autoCompleteRef) {
   let autoComplete = new window.google.maps.places.Autocomplete(
     autoCompleteRef.current,
     { types: ["geocode"], componentRestrictions: { country: "us" } }
   );
   autoComplete.setFields(["address_components", "formatted_address", "geometry"]);
   autoComplete.addListener("place_changed", () =>{
+
+    // Parse data from the Google Maps API object 
     const addressObject = autoComplete.getPlace();
-    const query = addressObject.formatted_address;
-    let zipCode; 
+    const address = addressObject.formatted_address;
+    let zipCode = ''; 
     if (addressObject.address_components[7]){
       zipCode = addressObject.address_components[7].long_name; 
-    }
+    } 
     const lat = addressObject.geometry.location.lat();
     const lng = addressObject.geometry.location.lng();
+    console.log(lat, lng);
 
-    let latlngStorage = JSON.parse(window.localStorage.getItem('latlngStorage')); 
-    let zipCodeStorage = JSON.parse(window.localStorage.getItem('zipCodeStorage'));
-    latlngStorage.push([lat, lng]);
-    if (zipCode && !zipCodeStorage.includes(zipCode)){
-      zipCodeStorage.push(zipCode);
-    }
-    window.localStorage.setItem('latlngStorage', JSON.stringify(latlngStorage)); 
-    window.localStorage.setItem('zipCodeStorage', JSON.stringify(zipCodeStorage));
-    console.log(window.localStorage); 
-    updateQuery(query);
+      // Add in data to the window storage objects 
+    window.localStorage.setItem('latlngStorage', JSON.stringify([lat, lng])); 
+    window.localStorage.setItem('zipCodeStorage', zipCode);
+    window.localStorage.setItem('addressStorage', address);
+
+    // Update automated value onto input form
+    updateAddress(address);
   });
 }
 
 export default function AutoCompleteAddressForm(props) {
-  const [query, setQuery] = useState("");
+  const [address, setAddress] = useState("");
   const autoCompleteRef = useRef(null);
 
   useEffect(() => {
     loadScript(
       `https://maps.googleapis.com/maps/api/js?key=AIzaSyDjrq68D6YESU7xYbaqIElmuOjFBVqJ3Rc&libraries=geometry,places`,
-      () => handleScriptLoad(setQuery, autoCompleteRef)
+      () => handleScriptLoad(setAddress, autoCompleteRef)
     );
   }, []);
 
@@ -70,9 +61,9 @@ export default function AutoCompleteAddressForm(props) {
     <div className="search-location-input">
       <input
         ref={autoCompleteRef}
-        onChange={event => setQuery(event.target.value)}
+        onChange={event => setAddress(event.target.value)}
         placeholder="Enter a location"
-        value={query}
+        value={address}
       />
     </div>
   );
--- src/home/Home.js
+++ src/home/Home.js
@@ -17,97 +17,68 @@ class Home extends React.Component {
     super(props);
     this.state = {
       storage: [],
-      address: "",
-      people: "",
       date: "",
       errorMessage: "",
     };
     // !IMPORTANT! bind all functions call to recognize this keyword
     this.handleChangeAddress = this.handleChangeAddress.bind(this);
-    this.handleChangePeople = this.handleChangePeople.bind(this);
     this.handleChangeDate = this.handleChangeDate.bind(this);
-    this.setDefaultAddress = this.setDefaultAddress.bind(this);
-    this.storeItem= this.storeItem.bind(this); 
     this.addItem = this.addItem.bind(this);
   }
 
   componentDidMount() {
-    // Load any prior stored address in local storage
-    Geocode.setApiKey("AIzaSyDjrq68D6YESU7xYbaqIElmuOjFBVqJ3Rc"); 
-    const localAddress =  window.localStorage.getItem("storage"); 
-    if (localAddress){
-      this.setState({
-        storage: JSON.parse(localAddress)
-      })
+    if (!window.localStorage.getItem('storage')){
+      window.localStorage.setItem('storage', JSON.stringify([]));
     }
-    // Get user current position as defualt address
-    const success = position => {
-      const latitude = position.coords.latitude;
-      const longitude = position.coords.longitude;
-      this.setDefaultAddress(latitude, longitude);
-    };
-    const error = () => {
-      console.log("Unable to retrieve your location");
-    };
-    navigator.geolocation.getCurrentPosition(success, error);
+    console.log('componentDidMount', window.localStorage);
   }
-  /* Set user default address based on their latitude and longitude */ 
-  setDefaultAddress(latitude, longitude){
-    Geocode.fromLatLng(latitude, longitude).then(
-      response => {
-        const a = response.results[0].formatted_address;
-        this.setState({
-          address: a
-        })
-      },
-      error => {
-        console.error(error);
-      });
-  }
+ 
   /* Handlers for input forms */ 
   handleChangeAddress(event) {
     this.setState({ address: event.target.value });
   }
-  handleChangePeople(event) {
-    this.setState({ people: event.target.value });
-  }
+ 
   handleChangeDate(event) {
     this.setState({ date: event.target.value });
   }
-  /* Store address item in local storage */ 
-  storeItem(key, val){
-    window.localStorage.setItem(key, JSON.stringify(val));
-  }
+  
   /* Add item to lists below input */ 
   addItem() {
-    if (this.state.address && this.state.date) {
-      const locationItem = {
-        address: this.state.address,
-        date: this.state.date,
-      };
-      const joined = this.state.storage.concat(locationItem);
-      this.setState({
-        storage: joined,
-        errorMessage: "",
-      }, function(){
-        this.storeItem("storage", this.state.storage);
-      });
-    } else {
-      const error = "Please fill in the address and date in the input";
-      this.setState({ errorMessage: error });
+    if (!this.state.date){
+      this.setState({ date: '08/21/2020' });
+    } 
+    window.localStorage.setItem('dateStorage', this.state.date);
+    this.setState({date: ''});
+    this.createItemArrayObject();
+  }
+
+  createItemArrayObject(){
+    let storage = JSON.parse(window.localStorage.getItem('storage'));
+    const object = {
+      'address': window.localStorage.getItem('addressStorage'),
+      'zipcode': window.localStorage.getItem('zipCodeStorage'),
+      'latlng': window.localStorage.getItem('latlngStorage'),
+      'date': window.localStorage.getItem('dateStorage'), 
     }
+    storage.push(object);
+    window.localStorage.setItem('storage', JSON.stringify(storage));
+    let keysToRemove = ['addressStorage', 'zipCodeStorage', 'latlngStorage', 'dateStorage']; 
+    for (let key of keysToRemove) {
+      window.localStorage.removeItem(key);
+    } 
+    console.log(window.localStorage);
   }
 
   render() {
-    const locationItems = this.state.storage.map((v) => {
-      return (
-        <LocationItem
-          key={v.address}
-          address={v.address}
-          date={v.date}
-        />
-      );
-    });
+    // const locationItems = this.state.storage.map((v) => {
+    //   return (
+    //     <LocationItem
+    //       key={v.address}
+    //       address={v.address}
+    //       date={v.date}
+    //     />
+    //   );
+    // });
 
     return (
       <div>
@@ -116,13 +87,13 @@ class Home extends React.Component {
           subtitle="Record location and people you've met."
         />
         <div className="Home">
-         <AutoCompleteAddressForm  />
+         <AutoCompleteAddressForm />
           <input type="date" onChange={this.handleChangeDate} />
           <button className="button left full" onClick={() => navigate("map")}> Visualize </button>
           <button className="button default left full" onClick={this.addItem}>
             Add Location
           </button>
-          <div className="timeline">{locationItems}</div>
+          {/* <div className="timeline">{locationItems}</div> */}
         </div>
       </div>
     );
